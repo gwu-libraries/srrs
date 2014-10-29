cb517d1fd3706456596fe0b6da5b28fb958f4ac6
Christian Aldridge <Studiozut@gmail.com>
2014-10-29 16:23:08 -0400

    Update Template.class.php
    Updated js for Universal analytics

--- lib/Template.class.php
+++ lib/Template.class.php
@@ -98,22 +98,17 @@ class Template {
 	    $("#machids").multiselect();
   	});
         </script>
-        
         <script>
 		// Google Analytics
-		var _gaq = _gaq || [];
-		_gaq.push(
-		['_setAccount', 'UA-45705339-1'],
-		['_trackPageview'],
-		['aggregate._setAccount', 'UA-45635686-1'],
-		['aggregate._trackPageview']
-		);
-		
-		(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  		ga('create', 'UA-45705339-1', 'auto');
+  		ga('create', 'UA-45635686-1', 'auto', {'name': 'aggregate'});  // sets aggregated Analytics account.
+  		ga('send', 'pageview');
+  		ga('aggregate.send', 'pageview'); // send page view for aggregated Analytics account.
 
 	</script>
         
